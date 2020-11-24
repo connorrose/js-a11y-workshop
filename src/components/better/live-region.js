58a49fb659e158d4f1519ceaf8e5d75ec5eebae7
Connor Rose <connorrose.git@gmail.com>
2020-11-24 14:50:39 -0800

    aria-live announcements

--- src/components/better/live-region.js
+++ src/components/better/live-region.js
@@ -1,9 +1,29 @@
 import React, {useState, useRef} from "react"
 
 const LiveRegion = () => {
+  const [message, setMessage] = useState('Nothing here.')
+  const inputRef = useRef(null)
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setMessage(inputRef.current.value);
+  }
 
   return (
     <>
+    <div status="status">
+      <p>
+        {message}
+      </p>
+    </div>
+    <form onSubmit={submitHandler}>
+      <label>
+        Enter text<br />
+        <input type="text" ref={inputRef} />
+      </label>
+      <button type="submit">
+        Start
+      </button>
+    </form>
     </>
   )
 }
